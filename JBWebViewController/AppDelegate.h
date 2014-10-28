7b22208e6033984cb0eab8c72a74ffab46103ab5
Jonas Boserup <jonas@iboserup.dk>
2014-10-28 15:19:02 +0100

    Initial Commit

---
+++ JBWebViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JBWebViewController
+//
+//  Created by Jonas Boserup on 28/10/14.
+//  Copyright (c) 2014 Jonas Boserup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
