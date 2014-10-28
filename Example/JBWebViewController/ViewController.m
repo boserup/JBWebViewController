11c225a95cfce068adcfa6ad58057b3eff89a181
Jonas Boserup <jonas@iboserup.dk>
2014-10-28 15:25:41 +0100

    Fixed project

---
+++ Example/JBWebViewController/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JBWebViewController
+//
+//  Created by Jonas Boserup on 28/10/14.
+//  Copyright (c) 2014 Jonas Boserup. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Example/JBWebViewController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JBWebViewController
+//
+//  Created by Jonas Boserup on 28/10/14.
+//  Copyright (c) 2014 Jonas Boserup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
