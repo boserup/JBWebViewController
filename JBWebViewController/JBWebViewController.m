05f0adc71fd8176eb128506353905f7231be11e1
Jonas Boserup <jonas@iboserup.dk>
2014-10-30 12:57:36 +0100

    Removed useless stuff

--- JBWebViewController/JBWebViewController.m
+++ JBWebViewController/JBWebViewController.m
@@ -34,14 +34,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    
-    self.view.backgroundColor = [UIColor redColor];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 - (void)viewWillAppear:(BOOL)animated
